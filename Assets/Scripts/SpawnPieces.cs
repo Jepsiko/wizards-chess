31f4cbe491622e8c4c9e1b1b3f7f5c5fdb9b7c73
Jepsiko <arthur.pierrot@hotmail.com>
2021-03-31 16:22:46 +0200

    Fixed queen and king is black down

--- Assets/Scripts/SpawnPieces.cs
+++ Assets/Scripts/SpawnPieces.cs
@@ -7,43 +7,48 @@ public class SpawnPieces : MonoBehaviour
 {
     public GameObject board;
     public GameObject[] pieces;
+    
+    private bool isWhiteDown;
 
     void Start()
     {
-        bool isWhite = board.GetComponent<DrawBoard>().isWhiteDown;
-        
+        isWhiteDown = board.GetComponent<DrawBoard>().isWhiteDown;
+
         // Spawn Pawns
         for (int file = 0; file < 8; file++)
         {
-            CreatePieceAtCoord(Piece.PieceType.Pawn, isWhite, file, 1);
-            CreatePieceAtCoord(Piece.PieceType.Pawn, !isWhite, file, 6);
+            CreatePieceAtCoord(Piece.PieceType.Pawn, isWhiteDown, file, 1);
+            CreatePieceAtCoord(Piece.PieceType.Pawn, !isWhiteDown, file, 6);
         }
-        
+
         // Spawn Rooks
-        CreatePieceAtCoord(Piece.PieceType.Rook, isWhite, 0, 0);
-        CreatePieceAtCoord(Piece.PieceType.Rook, isWhite, 7, 0);
-        CreatePieceAtCoord(Piece.PieceType.Rook, !isWhite, 0, 7);
-        CreatePieceAtCoord(Piece.PieceType.Rook, !isWhite, 7, 7);
-        
+        CreatePieceAtCoord(Piece.PieceType.Rook, isWhiteDown, 0, 0);
+        CreatePieceAtCoord(Piece.PieceType.Rook, isWhiteDown, 7, 0);
+        CreatePieceAtCoord(Piece.PieceType.Rook, !isWhiteDown, 0, 7);
+        CreatePieceAtCoord(Piece.PieceType.Rook, !isWhiteDown, 7, 7);
+
         // Spawn Knights
-        CreatePieceAtCoord(Piece.PieceType.Knight, isWhite, 1, 0);
-        CreatePieceAtCoord(Piece.PieceType.Knight, isWhite, 6, 0);
-        CreatePieceAtCoord(Piece.PieceType.Knight, !isWhite, 1, 7);
-        CreatePieceAtCoord(Piece.PieceType.Knight, !isWhite, 6, 7);
-        
+        CreatePieceAtCoord(Piece.PieceType.Knight, isWhiteDown, 1, 0);
+        CreatePieceAtCoord(Piece.PieceType.Knight, isWhiteDown, 6, 0);
+        CreatePieceAtCoord(Piece.PieceType.Knight, !isWhiteDown, 1, 7);
+        CreatePieceAtCoord(Piece.PieceType.Knight, !isWhiteDown, 6, 7);
+
         // Spawn Bishops
-        CreatePieceAtCoord(Piece.PieceType.Bishop, isWhite, 2, 0);
-        CreatePieceAtCoord(Piece.PieceType.Bishop, isWhite, 5, 0);
-        CreatePieceAtCoord(Piece.PieceType.Bishop, !isWhite, 2, 7);
-        CreatePieceAtCoord(Piece.PieceType.Bishop, !isWhite, 5, 7);
-        
+        CreatePieceAtCoord(Piece.PieceType.Bishop, isWhiteDown, 2, 0);
+        CreatePieceAtCoord(Piece.PieceType.Bishop, isWhiteDown, 5, 0);
+        CreatePieceAtCoord(Piece.PieceType.Bishop, !isWhiteDown, 2, 7);
+        CreatePieceAtCoord(Piece.PieceType.Bishop, !isWhiteDown, 5, 7);
+
+        int queenFile = isWhiteDown ? 3 : 4;
+        int kingFile = isWhiteDown ? 4 : 3;
+
         // Spawn Queens
-        CreatePieceAtCoord(Piece.PieceType.Queen, isWhite, 3, 0);
-        CreatePieceAtCoord(Piece.PieceType.Queen, !isWhite, 3, 7);
-        
+        CreatePieceAtCoord(Piece.PieceType.Queen, isWhiteDown, queenFile, 0);
+        CreatePieceAtCoord(Piece.PieceType.Queen, !isWhiteDown, queenFile, 7);
+
         // Spawn Kings
-        CreatePieceAtCoord(Piece.PieceType.King, isWhite, 4, 0);
-        CreatePieceAtCoord(Piece.PieceType.King, !isWhite, 4, 7);
+        CreatePieceAtCoord(Piece.PieceType.King, isWhiteDown, kingFile, 0);
+        CreatePieceAtCoord(Piece.PieceType.King, !isWhiteDown, kingFile, 7);
     }
 
     void CreatePieceAtCoord(Piece.PieceType type, bool isWhite, int file, int rank)
@@ -53,7 +58,7 @@ public class SpawnPieces : MonoBehaviour
 
         piece.GetComponent<DragAndDrop>().canvas = transform.parent.GetComponent<Canvas>();
 
-        Transform square = board.transform.Find(BoardNotation.SquareNameFromCoordinate(file, rank, true));
+        Transform square = board.transform.Find(BoardNotation.SquareNameFromCoordinate(file, rank, isWhiteDown));
         createdPiece.GetComponent<RectTransform>().anchoredPosition =
             square.GetComponent<RectTransform>().anchoredPosition;
     }
