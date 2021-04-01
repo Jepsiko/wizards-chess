9618f9d3e3099d9ca52ad999cc04d26c7c773341
Jepsiko <arthur.pierrot@hotmail.com>
2021-04-01 10:46:12 +0200

    Queen Movements added

--- Assets/Scripts/QueenMovements.cs
+++ Assets/Scripts/QueenMovements.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QueenMovements : MonoBehaviour
+public class QueenMovements : MonoBehaviour, IMovable
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<string> GetPossibleMoves()
     {
-        
+        List<string> possibleMoves = new List<string>();
+
+        string position = GetComponent<Piece>().position;
+
+        AddAlongLine(possibleMoves, position, -1, 0);
+        AddAlongLine(possibleMoves, position, 0, -1);
+        AddAlongLine(possibleMoves, position, +1, 0);
+        AddAlongLine(possibleMoves, position, 0, +1);
+
+        AddAlongLine(possibleMoves, position, -1, -1);
+        AddAlongLine(possibleMoves, position, -1, +1);
+        AddAlongLine(possibleMoves, position, +1, -1);
+        AddAlongLine(possibleMoves, position, +1, +1);
+
+        return possibleMoves;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddAlongLine(List<string> possibleMoves, string position, int fileOffset, int rankOffset)
     {
+        string nextPosition = BoardNotation.MoveFileAndRank(position, fileOffset, rankOffset);
+        while (nextPosition != null && !GameController.Instance.IsOccupied(nextPosition))
+        {
+            possibleMoves.Add(nextPosition);
+            nextPosition = BoardNotation.MoveFileAndRank(nextPosition, fileOffset, rankOffset);
+        }
+    }
+
+    public List<string> GetLegalMoves()
+    {
+        List<string> possibleMoves = GetPossibleMoves();
+        List<string> legalMoves = new List<string>();
+
+        foreach (string possibleMove in possibleMoves)
+        {
+            if (!GameController.Instance.IsOccupied(possibleMove))
+                legalMoves.Add(possibleMove);
+        }
         
+        return legalMoves;
     }
 }
