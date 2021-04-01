66a93ef14cbca600d42a0ef2d48f8c28b3550e26
Jepsiko <arthur.pierrot@hotmail.com>
2021-04-01 10:49:54 +0200

    King Movements added

--- Assets/Scripts/KingMovements.cs
+++ Assets/Scripts/KingMovements.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KingMovements : MonoBehaviour
+public class KingMovements : MonoBehaviour, IMovable
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<string> GetPossibleMoves()
     {
-        
+        List<string> possibleMoves = new List<string>();
+
+        string position = GetComponent<Piece>().position;
+
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i != 0 || j != 0)
+                {
+                    string nextPosition = BoardNotation.MoveFileAndRank(position, i, j);
+                    if (nextPosition != null && !GameController.Instance.IsOccupied(nextPosition))
+                    {
+                        possibleMoves.Add(nextPosition);
+                    }
+                }
+            }
+        }
+
+        return possibleMoves;
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<string> GetLegalMoves()
     {
+        List<string> possibleMoves = GetPossibleMoves();
+        List<string> legalMoves = new List<string>();
+
+        foreach (string possibleMove in possibleMoves)
+        {
+            if (!GameController.Instance.IsOccupied(possibleMove))
+                legalMoves.Add(possibleMove);
+        }
         
+        return legalMoves;
     }
 }
